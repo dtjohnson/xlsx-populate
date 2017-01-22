685e2952930167fad20be596fda095acdd0f0b4b
Dave Johnson <dave@dtjohnson.net>
2017-01-22 14:28:10 -0500

    Even more style fixes.

--- examples/shared-formulas/index.js
+++ examples/shared-formulas/index.js
@@ -8,8 +8,8 @@ Workbook.fromFileAsync('./template.xlsx')
     .then(workbook => {
         // Randomly generate 10 rows of data.
         const sheet = workbook.sheet('ClickThroughRateSheet');
-        sheet.range("B3:B13").iterateCells(cell => cell.value(parseInt(1e3 * Math.random())));
-        sheet.range("C3:C13").iterateCells(cell => cell.value(parseInt(1e6 * Math.random())));
+        sheet.range("B3:B13").forEach(cell => cell.value(parseInt(1e3 * Math.random())));
+        sheet.range("C3:C13").forEach(cell => cell.value(parseInt(1e6 * Math.random())));
         sheet.range("D3:D13").formula("B3/C3");
 
         // Write to file.
