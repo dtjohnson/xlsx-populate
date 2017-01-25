6e04e08a610d6067878b7e71b4886c7b92a9f597
Dave Johnson <dave@dtjohnson.net>
2017-01-24 19:53:48 -0500

    Cleaned up gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,16 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
+const eslint = require("gulp-eslint");
+const jasmine = require("gulp-jasmine");
+const runSequence = require('run-sequence').use(gulp);
 
 const BROWSERIFY_STANDALONE_NAME = "XLSXPopulate";
 const BABEL_PRESETS = ["es2015"];
 const PATHS = {
+    lib: "./lib/**/*.js",
+    spec: "./spec/**/*.js",
+    examples: "./examples/**/*.js",
     browserify: {
         source: "./lib/browser.js",
         base: "./browser",
@@ -20,6 +26,9 @@ const PATHS = {
     }
 };
 
+PATHS.lint = [PATHS.lib];
+PATHS.testSources = [PATHS.lib, PATHS.spec];
+
 gulp.task('build', () => {
     return browserify({
         entries: PATHS.browserify.source,
@@ -27,99 +36,44 @@ gulp.task('build', () => {
         standalone: BROWSERIFY_STANDALONE_NAME
     })
         .transform("babelify", { presets: BABEL_PRESETS })
+        .transform("brfs")
         .bundle()
-        .pipe(source(PATHS.browserify.base))
+        .pipe(source(PATHS.browserify.bundle))
         .pipe(buffer())
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
         .pipe(sourcemaps.write(PATHS.browserify.sourceMap))
-        .pipe(gulp.dest(PATHS.browserify.bundle));
+        .pipe(gulp.dest(PATHS.browserify.base));
 });
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch('./lib/**/*.js', ['build']);
+gulp.task("lint", () => {
+    return gulp
+        .src(PATHS.lint)
+        .pipe(eslint())
+        .pipe(eslint.format());
 });
 
-gulp.task('default', ['watch']);
+gulp.task("unit", () => {
+    return gulp
+        .src(PATHS.spec)
+        .pipe(jasmine({
+            includeStackTrace: false,
+            errorOnFail: false
+        }));
+});
 
-//
-// var gulp = require("gulp");
-// var eslint = require("gulp-eslint");
-// var jasmine = require("gulp-jasmine");
-// var runSequence = require('run-sequence').use(gulp);
-//
-// var sourcemaps = require('gulp-sourcemaps');
-// var source = require('vinyl-source-stream');
-// var buffer = require('vinyl-buffer');
-// var browserify = require('browserify');
-// var watchify = require('watchify');
-// var babel = require('babelify');
-//
-//
-// function compile(watch) {
-//     var bundler = watchify(browserify('./lib/browser.js', { debug: true, standalone: "XLSXPopulate" }).transform(babel, { presets: ["es2015"]}));
-//
-//     function rebundle() {
-//         bundler.bundle()
-//             .on('error', function(err) { console.error(err); this.emit('end'); })
-//             .pipe(source('xlsx-populate.js'))
-//             .pipe(buffer())
-//             .pipe(sourcemaps.init({ loadMaps: true }))
-//             .pipe(sourcemaps.write('./'))
-//             .pipe(gulp.dest('./browser'));
-//     }
-//
-//     if (watch) {
-//         bundler.on('update', function() {
-//             console.log('-> bundling...');
-//             rebundle();
-//         });
-//     }
-//
-//     rebundle();
-// }
-//
-// function watch() {
-//     return compile(true);
-// };
-//
-// gulp.task('build', function() { return compile(); });
-// gulp.task('watch', function() { return watch(); });
-//
-// gulp.task('default', ['watch']);
 
-// var TEST = "spec/**/*.spec.js";
-// var LIB = "lib/**/*.js";
-// var EXAMPLES = "examples/**/*.js";
-// var SRC = [LIB, TEST, EXAMPLES];
-//
-//
-// gulp.task("lint", function () {
-//     return gulp
-//         .src(SRC)
-//         .pipe(eslint())
-//         .pipe(eslint.format());
-// });
-//
-// gulp.task("unit", function () {
-//     return gulp
-//         .src(TEST)
-//         .pipe(jasmine({
-//             includeStackTrace: false,
-//             errorOnFail: false
-//         }));
-// });
-//
-// gulp.task("test", function (cb) {
-//     // Use run sequence to make sure lint and unit run in series. They both output to the
-//     // console so parallel execution would lead to some funny output.
-//     runSequence("unit", cb);//"lint"
-// });
-//
-// gulp.task("watch", function () {
-//     gulp.watch(SRC, ["test"]);
-// });
-//
-// gulp.task("default", function (cb) {
-//     runSequence("test", "watch", cb);
-// });
+gulp.task("test", cb => {
+    // Use run sequence to make sure lint and unit run in series. They both output to the
+    // console so parallel execution would lead to some funny output.
+    runSequence("unit", cb);//"lint"
+});
+
+gulp.task('watch', ['build'], () => {
+    gulp.watch(PATHS.lib, ['build']);
+    gulp.watch(PATHS.testSources, ["test"]);
+});
+
+gulp.task("default", cb => {
+    runSequence(["build", "test"], "watch", cb);
+});
