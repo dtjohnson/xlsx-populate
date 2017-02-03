a1ce1db65bbca87a1efbdfbd4e2b3a621132d456
Dave Johnson <dave@dtjohnson.net>
2017-02-02 20:53:37 -0500

    Tested _StyleSheet.

--- spec/_StyleSheet.spec,js.js
+++ spec/_StyleSheet.spec,js.js
@@ -1,94 +1,242 @@
 "use strict";
 
 const proxyquire = require("proxyquire").noCallThru();
-const DOMParser = require('xmldom').DOMParser;
-const parser = new DOMParser();
-const xq = require("../lib/xq");
 
-return;
-xdescribe("_StyleSheet", () => {
-    let _Style, _StyleSheet, styleSheet, styleSheetText;
+describe("_StyleSheet", () => {
+    let _Style, _StyleSheet, styleSheet, styleSheetNode;
 
     beforeEach(() => {
         _Style = jasmine.createSpy("_Style");
         _StyleSheet = proxyquire("../lib/_StyleSheet", {
             "./_Style": _Style
         });
-        styleSheetText = `
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main">
-    <fonts count="1" x14ac:knownFonts="1"></fonts>
-    <fills count="11"></fills>
-    <borders count="10"><border foo="bar"/></borders>
-    <cellStyleXfs count="1">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
-    </cellStyleXfs>
-    <cellXfs count="19">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyBorder="1" xfId="0"/>
-    </cellXfs>
-</styleSheet>`;
-        styleSheet = new _StyleSheet(styleSheetText);
-    });
 
-    describe("constructor", () => {
-        fit("should initialize", () => {
-            expect(xq.query(styleSheet._xml.documentElement, {
-                fonts: { '@count': 0 },
-                fills: { '@count': 0 },
-                borders: { '@count': 1 },
-                cellXfs: { '@count': 1 }
-            })).toBeTruthy();
-            /*expect(styleSheet._xml.toString()).toBe(`
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main">
-    <fonts count="0" x14ac:knownFonts="1"/>
-    <fills count="0"/>
-    <borders count="1"><border foo="bar"/></borders>
-    <cellStyleXfs count="1">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
-    </cellStyleXfs>
-    <cellXfs count="1">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyBorder="1" xfId="0"/>
-    </cellXfs>
-</styleSheet>`);*/
-        });
+        styleSheetNode = {
+            styleSheet: {
+                $: {
+                    xmlns: "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+                },
+                fonts: [{
+                    $: {
+                        count: 1,
+                        'x14ac:knownFonts': 1
+                    },
+                    font: []
+                }],
+                fills: [{
+                    $: {
+                        count: 11
+                    },
+                    fill: []
+                }],
+                borders: [{
+                    $: {
+                        count: 10
+                    },
+                    border: [{
+                        $: {
+                            foo: "bar"
+                        }
+                    }]
+                }],
+                cellStyleXfs: [{
+                    $: {
+                        count: 1
+                    },
+                    xf: [{
+                        $: {
+                            numFmtId: 0,
+                            fontId: 0,
+                            fillId: 0,
+                            borderId: 0
+                        }
+                    }]
+                }],
+                cellXfs: [{
+                    $: {
+                        count: 19
+                    },
+                    xf: [{
+                        $: {
+                            numFmtId: 0,
+                            fontId: 0,
+                            fillId: 0,
+                            borderId: 0,
+                            applyBorder: 1,
+                            xfId: 0
+                        }
+                    }]
+                }]
+            }
+        };
+        styleSheet = new _StyleSheet(styleSheetNode);
     });
 
     describe("createStyle", () => {
         it("should clone an existing style", () => {
             const style = styleSheet.createStyle(0);
-            expect(style).toEqual(jasmine.any(_Style));
-            expect(styleSheet._xml.toString()).toBe(`
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main"><numFmts/>
-    <fonts count="1" x14ac:knownFonts="1"><font/></fonts>
-    <fills count="1"><fill/></fills>
-    <borders count="2"><border foo="bar"/><border foo="bar"/></borders>
-    <cellStyleXfs count="1">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
-    </cellStyleXfs>
-    <cellXfs count="2">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyBorder="1" xfId="0"/>
-    <xf numFmtId="0" fontId="0" fillId="0" borderId="1" applyBorder="1" xfId="0" applyFont="1" applyFill="1"/></cellXfs>
-</styleSheet>`);
+            expect(style.constructor).toBe(_Style);
+            expect(styleSheet._node).toEqualJson({
+                styleSheet: {
+                    $: {
+                        xmlns: "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+                    },
+                    numFmts: [{
+                        numFmt: []
+                    }],
+                    fonts: [{
+                        $: {
+                            'x14ac:knownFonts': 1
+                        },
+                        font: [
+                            {}
+                        ]
+                    }],
+                    fills: [{
+                        $: {},
+                        fill: [
+                            {}
+                        ]
+                    }],
+                    borders: [{
+                        $: {},
+                        border: [{
+                            $: {
+                                foo: "bar"
+                            }
+                        }, {
+                            $: {
+                                foo: "bar"
+                            }
+                        }]
+                    }],
+                    cellStyleXfs: [{
+                        $: {
+                            count: 1
+                        },
+                        xf: [{
+                            $: {
+                                numFmtId: 0,
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 0
+                            }
+                        }]
+                    }],
+                    cellXfs: [{
+                        $: {},
+                        xf: [{
+                            $: {
+                                numFmtId: 0,
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 0,
+                                applyBorder: 1,
+                                xfId: 0
+                            }
+                        }, {
+                            $: {
+                                numFmtId: 0,
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 1,
+                                applyFill: 1,
+                                applyFont: 1,
+                                applyBorder: 1,
+                                xfId: 0
+                            }
+                        }]
+                    }]
+                }
+            });
+            expect(_Style).toHaveBeenCalledWith(
+                styleSheet,
+                1,
+                styleSheetNode.styleSheet.cellXfs[0].xf[1],
+                styleSheetNode.styleSheet.fonts[0].font[0],
+                styleSheetNode.styleSheet.fills[0].fill[0],
+                styleSheetNode.styleSheet.borders[0].border[1]
+            );
         });
 
         it("should create a new style", () => {
             const style = styleSheet.createStyle(undefined);
-            expect(style).toEqual(jasmine.any(_Style));
-            expect(styleSheet._xml.toString()).toBe(`
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main"><numFmts/>
-    <fonts count="1" x14ac:knownFonts="1"><font/></fonts>
-    <fills count="1"><fill/></fills>
-    <borders count="2"><border foo="bar"/><border/></borders>
-    <cellStyleXfs count="1">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0"/>
-    </cellStyleXfs>
-    <cellXfs count="2">
-        <xf numFmtId="0" fontId="0" fillId="0" borderId="0" applyBorder="1" xfId="0"/>
-    <xf fontId="0" fillId="0" borderId="1" applyFont="1" applyFill="1" applyBorder="1"/></cellXfs>
-</styleSheet>`);
+            expect(style.constructor).toBe(_Style);
+            expect(styleSheet._node).toEqualJson({
+                styleSheet: {
+                    $: {
+                        xmlns: "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+                    },
+                    numFmts: [{
+                        numFmt: []
+                    }],
+                    fonts: [{
+                        $: {
+                            'x14ac:knownFonts': 1
+                        },
+                        font: [
+                            {}
+                        ]
+                    }],
+                    fills: [{
+                        $: {},
+                        fill: [
+                            {}
+                        ]
+                    }],
+                    borders: [{
+                        $: {},
+                        border: [{
+                            $: {
+                                foo: "bar"
+                            }
+                        }, {}]
+                    }],
+                    cellStyleXfs: [{
+                        $: {
+                            count: 1
+                        },
+                        xf: [{
+                            $: {
+                                numFmtId: 0,
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 0
+                            }
+                        }]
+                    }],
+                    cellXfs: [{
+                        $: {},
+                        xf: [{
+                            $: {
+                                numFmtId: 0,
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 0,
+                                applyBorder: 1,
+                                xfId: 0
+                            }
+                        }, {
+                            $: {
+                                fontId: 0,
+                                fillId: 0,
+                                borderId: 1,
+                                applyFill: 1,
+                                applyFont: 1,
+                                applyBorder: 1
+                            }
+                        }]
+                    }]
+                }
+            });
+            expect(_Style).toHaveBeenCalledWith(
+                styleSheet,
+                1,
+                styleSheetNode.styleSheet.cellXfs[0].xf[1],
+                styleSheetNode.styleSheet.fonts[0].font[0],
+                styleSheetNode.styleSheet.fills[0].fill[0],
+                styleSheetNode.styleSheet.borders[0].border[1]
+            );
         });
     });
 
@@ -106,16 +254,45 @@ xdescribe("_StyleSheet", () => {
         });
 
         it("should add a custom format node if code doesn't exist", () => {
-            expect(styleSheet._numFmtsNode.toString()).toBe('<numFmts/>');
+            expect(styleSheet._numFmtsNode).toEqualJson({ numFmt: [] });
             expect(styleSheet.getNumberFormatId('foo')).toBe(164);
-            expect(styleSheet._numFmtsNode.toString()).toBe('<numFmts><numFmt numFmtId="164" formatCode="foo"/></numFmts>');
+            expect(styleSheet._numFmtsNode).toEqualJson({
+                numFmt: [{
+                    $: {
+                        formatCode: "foo",
+                        numFmtId: 164
+                    }
+                }]
+            });
         });
     });
 
-    describe("toString", () => {
-        it("should export to the XML string", () => {
-            styleSheet._xml = parser.parseFromString("<foo/>");
-            expect(styleSheet.toString()).toBe('<foo/>');
+    describe("toObject", () => {
+        it("should return the node as is", () => {
+            expect(styleSheet.toObject()).toBe(styleSheetNode);
+        });
+    });
+
+    describe("_cacheNumberFormats", () => {
+        it("should cache the number formats", () => {
+            styleSheet.getNumberFormatId("foo");
+            styleSheet._cacheNumberFormats();
+            expect(styleSheet._numberFormatCodesById[0]).toBe("General");
+            expect(styleSheet._numberFormatCodesById[49]).toBe("@");
+            expect(styleSheet._numberFormatCodesById[164]).toBe("foo");
+            expect(styleSheet._numberFormatIdsByCode[`General`]).toBe(0);
+            expect(styleSheet._numberFormatIdsByCode[`@`]).toBe(49);
+            expect(styleSheet._numberFormatIdsByCode[`foo`]).toBe(164);
+        });
+    });
+
+    describe("_initNode", () => {
+        it("should add the numFmts node and clear the counts", () => {
+            expect(styleSheetNode.styleSheet.numFmts).toEqualJson([{ numFmt: [] }]);
+            expect(styleSheetNode.styleSheet.fonts[0].$.count).toBeUndefined();
+            expect(styleSheetNode.styleSheet.fills[0].$.count).toBeUndefined();
+            expect(styleSheetNode.styleSheet.borders[0].$.count).toBeUndefined();
+            expect(styleSheetNode.styleSheet.cellXfs[0].$.count).toBeUndefined();
         });
     });
 });
