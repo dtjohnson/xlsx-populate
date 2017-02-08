95bf76e04b19cd44dd607234d6ec9327e5361a4b
Dave Johnson <dave@dtjohnson.net>
2017-02-08 07:13:39 -0500

    Work on getting the final xml2js migration done. Currently broken.

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -3,10 +3,10 @@
 const Workbook = require('../../lib/Workbook');
 
 // Load the input workbook from file.
-Workbook.fromBlankAsync()
+Workbook.fromFileAsync("../shared-formulas/template.xlsx")
     .then(workbook => {
         // Modify the workbook.
-        workbook.sheet("Sheet1").cell("A1").value("This is neat!").value();
+        // workbook.sheet("Sheet1").cell("A1").value("This is neat!").value();
 
         // Write to file.
         return workbook.toFileAsync("./out.xlsx");
