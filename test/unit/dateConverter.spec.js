d631e28a8f60fa21eb3f2ea8d5414d6d4d4ebefa
Dave Johnson <dave@dtjohnson.net>
2017-03-28 07:39:36 -0400

    Fixed dateConverter tests.

--- test/unit/dateConverter.spec.js
+++ test/unit/dateConverter.spec.js
@@ -22,11 +22,11 @@ describe("dateConverter", () => {
 
     describe("numberToDate", () => {
         it("should convert number to date", () => {
-            expect(dateConverter.numberToDate(1)).toEqual(new Date('1900-01-01T05:00:00.000Z'));
-            expect(dateConverter.numberToDate(59)).toEqual(new Date('1900-02-28T05:00:00.000Z'));
-            expect(dateConverter.numberToDate(61)).toEqual(new Date('1900-03-01T05:00:00.000Z'));
-            expect(dateConverter.numberToDate(42070.56)).toEqual(new Date('2015-03-07T18:26:24.000Z'));
-            expect(dateConverter.numberToDate(42829.8333333333)).toEqual(new Date('2017-04-05T01:00:00.000Z'));
+            expect(dateConverter.numberToDate(1)).toEqual(new Date('01 Jan 1900 00:00:00'));
+            expect(dateConverter.numberToDate(59)).toEqual(new Date('28 Feb 1900 00:00:00'));
+            expect(dateConverter.numberToDate(61)).toEqual(new Date('01 Mar 1900 00:00:00'));
+            expect(dateConverter.numberToDate(42070.56)).toEqual(new Date('07 Mar 2015 13:26:24'));
+            expect(dateConverter.numberToDate(42829.8333333333)).toEqual(new Date('04 Apr 2017 21:00:00'));
         });
     });
 });
