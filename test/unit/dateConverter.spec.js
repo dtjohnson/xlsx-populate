4163f2dfae923a55308c65771a5ae9b26cb282d3
Dave Johnson <dave@dtjohnson.net>
2017-03-28 20:58:55 -0400

    Rewrote date converter functions to correct daylight savings bug.

--- test/unit/dateConverter.spec.js
+++ test/unit/dateConverter.spec.js
@@ -17,6 +17,7 @@ describe("dateConverter", () => {
             expect(dateConverter.dateToNumber(new Date('28 Feb 1900 00:00:00'))).toBe(59);
             expect(dateConverter.dateToNumber(new Date('01 Mar 1900 00:00:00'))).toBe(61);
             expect(dateConverter.dateToNumber(new Date('07 Mar 2015 13:26:24'))).toBe(42070.56);
+            expect(dateConverter.dateToNumber(new Date('04 Apr 2017 20:00:00'))).toBeCloseTo(42829.8333333333, 10);
         });
     });
 
@@ -26,7 +27,7 @@ describe("dateConverter", () => {
             expect(dateConverter.numberToDate(59)).toEqual(new Date('28 Feb 1900 00:00:00'));
             expect(dateConverter.numberToDate(61)).toEqual(new Date('01 Mar 1900 00:00:00'));
             expect(dateConverter.numberToDate(42070.56)).toEqual(new Date('07 Mar 2015 13:26:24'));
-            expect(dateConverter.numberToDate(42829.8333333333)).toEqual(new Date('04 Apr 2017 21:00:00'));
+            expect(dateConverter.numberToDate(42829.8333333333)).toEqual(new Date('04 Apr 2017 20:00:00'));
         });
     });
 });
