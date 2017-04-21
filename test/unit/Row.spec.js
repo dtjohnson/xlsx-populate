0a0985246019164773ed4916c7ef3d4e4f56dbb2
Dave Johnson <dave@dtjohnson.net>
2017-04-21 07:36:49 -0400

    Fixed Row tests.

--- test/unit/Row.spec.js
+++ test/unit/Row.spec.js
@@ -1,9 +1,10 @@
 "use strict";
 
+const _ = require('lodash');
 const proxyquire = require("proxyquire");
 
 describe("Row", () => {
-    let Row, Cell, row, rowNode, sheet;
+    let Row, Cell, row, rowNode, sheet, style, styleSheet, workbook;
 
     beforeEach(() => {
         let i = 1;
@@ -15,14 +16,29 @@ describe("Row", () => {
             return this.id;
         });
         Cell.prototype.find = jasmine.createSpy('find');
+        Cell.prototype.style = jasmine.createSpy('style');
 
         Row = proxyquire("../../lib/Row", {
             './Cell': Cell,
             '@noCallThru': true
         });
-        sheet = jasmine.createSpyObj('sheet', ['name', 'workbook', 'existingColumnStyleId']);
+
+
+        style = jasmine.createSpyObj("style", ["style", "id"]);
+        style.id.and.returnValue("STYLE_ID");
+        style.style.and.callFake(name => `STYLE:${name}`);
+
+        styleSheet = jasmine.createSpyObj("styleSheet", ["createStyle"]);
+        styleSheet.createStyle.and.returnValue(style);
+
+        workbook = jasmine.createSpyObj("workbook", ["sharedStrings", "styleSheet"]);
+        workbook.styleSheet.and.returnValue(styleSheet);
+
+        sheet = jasmine.createSpyObj('sheet', ['name', 'workbook', 'existingColumnStyleId', 'forEachExistingColumnNumber']);
         sheet.name.and.returnValue('NAME');
-        sheet.workbook.and.returnValue('WORKBOOK');
+        sheet.workbook.and.returnValue(workbook);
+        sheet.existingColumnStyleId.and.callFake(columnNumber => columnNumber === 4 ? "STYLE_ID" : undefined);
+        sheet.forEachExistingColumnNumber.and.callFake(callback => _.forEach([1, 2, 4], callback));
 
         rowNode = {
             name: 'row',
@@ -39,6 +55,8 @@ describe("Row", () => {
 
         row = new Row(sheet, rowNode);
     });
+
+    /* PUBLIC */
 
     describe("address", () => {
         it("should return the address", () => {
@@ -65,33 +83,36 @@ describe("Row", () => {
         });
 
         it("should create a new cell as needed", () => {
-            expect(row.cell(5)).toEqual(jasmine.any(Cell));
-            expect(Cell).toHaveBeenCalledWith(row, {
-                name: 'c', attributes: { r: "E7" }, children: []
-            });
+            const cell = row.cell(5);
+            expect(cell).toEqual(jasmine.any(Cell));
+            expect(Cell).toHaveBeenCalledWith(row, 5, undefined);
+            expect(row._cells[5]).toBe(cell);
         });
 
         it("should create a new cell as needed", () => {
-            expect(row.cell('C')).toEqual(jasmine.any(Cell));
-            expect(Cell).toHaveBeenCalledWith(row, {
-                name: 'c', attributes: { r: "C7" }, children: []
-            });
+            const cell = row.cell('C');
+            expect(cell).toEqual(jasmine.any(Cell));
+            expect(Cell).toHaveBeenCalledWith(row, 3, undefined);
+            expect(row._cells[3]).toBe(cell);
         });
 
         it("should create a new cell with an existing column style id", () => {
             sheet.existingColumnStyleId.and.returnValue(5);
             expect(row.cell('C')).toEqual(jasmine.any(Cell));
-            expect(Cell).toHaveBeenCalledWith(row, {
-                name: 'c', attributes: { r: "C7", s: 5 }, children: []
-            });
+            expect(Cell).toHaveBeenCalledWith(row, 3, 5);
         });
 
         it("should create a new cell with an existing row style id", () => {
             rowNode.attributes.s = 3;
             expect(row.cell('C')).toEqual(jasmine.any(Cell));
-            expect(Cell).toHaveBeenCalledWith(row, {
-                name: 'c', attributes: { r: "C7", s: 3 }, children: []
-            });
+            expect(Cell).toHaveBeenCalledWith(row, 3, 3);
+        });
+
+        it("should create a new cell with an existing row and column style id", () => {
+            sheet.existingColumnStyleId.and.returnValue(5);
+            rowNode.attributes.s = 3;
+            expect(row.cell('C')).toEqual(jasmine.any(Cell));
+            expect(Cell).toHaveBeenCalledWith(row, 3, 3);
         });
     });
 
@@ -146,6 +167,66 @@ describe("Row", () => {
         });
     });
 
+    describe("style", () => {
+        beforeEach(() => {
+            spyOn(row, "_createStyleIfNeeded");
+            row._style = style;
+        });
+
+        it("should get a single style", () => {
+            expect(row.style("foo")).toBe("STYLE:foo");
+            expect(style.style).toHaveBeenCalledWith("foo");
+            expect(row._createStyleIfNeeded).toHaveBeenCalledWith();
+        });
+
+        it("should get multiple styles", () => {
+            expect(row.style(["foo", "bar", "baz"])).toEqualJson({
+                foo: "STYLE:foo", bar: "STYLE:bar", baz: "STYLE:baz"
+            });
+            expect(style.style).toHaveBeenCalledWith("foo");
+            expect(style.style).toHaveBeenCalledWith("bar");
+            expect(style.style).toHaveBeenCalledWith("baz");
+            expect(row._createStyleIfNeeded).toHaveBeenCalledWith();
+        });
+
+        it("should set a single style", () => {
+            expect(row._cells[2]).toBeDefined();
+            expect(row._cells[4]).toBeUndefined();
+
+            expect(row.style("foo", "value")).toBe(row);
+            expect(style.style).toHaveBeenCalledWith("foo", "value");
+
+            expect(row._cells[1]).toBeUndefined();
+            expect(row._cells[2].style).toHaveBeenCalledWith("foo", "value");
+            expect(row._cells[3]).toBeUndefined();
+            expect(row._cells[4].style).toHaveBeenCalledWith("foo", "value");
+        });
+
+        it("should set multiple styles", () => {
+            expect(row.style({
+                foo: "FOO", bar: "BAR", baz: "BAZ"
+            })).toBe(row);
+            expect(style.style).toHaveBeenCalledWith("foo", "FOO");
+            expect(style.style).toHaveBeenCalledWith("bar", "BAR");
+            expect(style.style).toHaveBeenCalledWith("baz", "BAZ");
+
+            expect(row._cells[2].style).toHaveBeenCalledWith("foo", "FOO");
+            expect(row._cells[2].style).toHaveBeenCalledWith("bar", "BAR");
+            expect(row._cells[2].style).toHaveBeenCalledWith("baz", "BAZ");
+            expect(row._cells[4].style).toHaveBeenCalledWith("foo", "FOO");
+            expect(row._cells[4].style).toHaveBeenCalledWith("bar", "BAR");
+            expect(row._cells[4].style).toHaveBeenCalledWith("baz", "BAZ");
+        });
+    });
+
+    describe("workbook", () => {
+        it("should return the workbook", () => {
+            expect(row.workbook()).toBe(workbook);
+        });
+    });
+
+    /* INTERNAL */
+
     describe("clearCellsUsingSharedFormula", () => {
         it("should clear cells with matching shared formula", () => {
             row._cells = [
@@ -181,6 +262,22 @@ describe("Row", () => {
         });
     });
 
+    describe("hasCell", () => {
+        it("should return true/false if the cell exists or not", () => {
+            expect(row.hasCell(1)).toBe(false);
+            expect(row.hasCell(2)).toBe(true);
+            expect(row.hasCell(3)).toBe(false);
+        });
+    });
+
+    describe("hasStyle", () => {
+        it("should return true/false if the row has a style set", () => {
+            expect(row.hasStyle()).toBe(false);
+            rowNode.attributes.s = 0;
+            expect(row.hasStyle()).toBe(true);
+        });
+    });
+
     describe("minUsedColumnNumber", () => {
         it("should return the min column number", () => {
             row._cells = [];
@@ -203,9 +300,23 @@ describe("Row", () => {
         });
     });
 
-    describe("workbook", () => {
-        it("should return the workbook", () => {
-            expect(row.workbook()).toBe("WORKBOOK");
+    /* PRIVATE */
+
+    describe("_createStyleIfNeeded", () => {
+        it("should create a style", () => {
+            rowNode.attributes.s = 3;
+            row._createStyleIfNeeded();
+            expect(row._style).toBe(style);
+            expect(rowNode.attributes.s).toBe("STYLE_ID");
+            expect(styleSheet.createStyle).toHaveBeenCalledWith(3);
+        });
+
+        it("should NOT create a style", () => {
+            const existingStyle = {};
+            row._style = existingStyle;
+            row._createStyleIfNeeded();
+            expect(row._style).toBe(existingStyle);
+            expect(styleSheet.createStyle).not.toHaveBeenCalled();
         });
     });
 
