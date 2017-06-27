83c7a9252278c15b8be9df8af5df7affd21cf562
Dave Johnson <dave@dtjohnson.net>
2017-06-27 07:37:34 -0400

    Reset lastIndex in regexify to prevent global state issue.

--- test/unit/regexify.spec.js
+++ test/unit/regexify.spec.js
@@ -11,9 +11,13 @@ describe("regexify", () => {
         });
     });
 
-    it("should return a regexp unchanged", () => {
+    it("should return the regex with lastIndex reset", () => {
         const regexp = /.+/;
-        expect(regexify(regexp)).toBe(regexp);
+        regexp.lastIndex = 5;
+
+        const actual = regexify(regexp);
+        expect(actual).toBe(regexp);
+        expect(actual.lastIndex).toBe(0);
     });
 
     it("should convert a string to a regexp", () => {
