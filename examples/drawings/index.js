e692ea87527e84b7776ffe13655b324a73d5d3da
Alan Doyle <code@thenerdstuff.com>
2018-01-20 00:19:46 -0800

    started to work on creating a Class for drawings, and loading data in so we can save it back to the workbooks.

--- examples/drawings/index.js
+++ examples/drawings/index.js
@@ -6,10 +6,5 @@ const fs = require('fs');
 
 // Load the input workbook from file.
 XlsxPopulate.fromFileAsync("in.xlsx").then(workbook => {
-    var tmpzip = new JSZip();
-    tmpzip.file('image', fs.readFileSync('new_image.png', {encoding: 'binary'})).generateAsync({type : "string"}).then(() => {
-        workbook.swapImage('test_image', tmpzip.files.image._data);
-        workbook.toFileAsync("./out.xlsx")
-    })
 })
 .catch(err => console.error(err));
