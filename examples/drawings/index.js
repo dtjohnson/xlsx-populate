22d8f631282b95e30a3f3a6b2fd81a84c45da897
Alan Doyle <code@thenerdstuff.com>
2018-01-21 01:33:08 -0800

    added in xml building. on to testing now.

--- examples/drawings/index.js
+++ examples/drawings/index.js
@@ -6,5 +6,6 @@ const fs = require('fs');
 
 // Load the input workbook from file.
 XlsxPopulate.fromFileAsync("in.xlsx").then(workbook => {
+    return workbook.toFileAsync('out.xlsx')
 })
 .catch(err => console.error(err));
