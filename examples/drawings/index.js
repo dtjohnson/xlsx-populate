899990101167e54e4ec552c6608e8a7dddd6b2c9
Alan Doyle <code@thenerdstuff.com>
2018-01-24 16:28:24 -0800

    fixed Drawings writing to back to files overriding non Image relationships and drawing objects.

--- examples/drawings/index.js
+++ examples/drawings/index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 
 // Load the input workbook from file.
 XlsxPopulate.fromFileAsync("in.xlsx").then(workbook => {
+    workbook.sheet(0).drawings('Test Image').name('new_test_image')
+    workbook.sheet(3).drawings('Picture 2').name('new_test_image_2')
     return workbook.toFileAsync('out.xlsx')
 })
 .catch(err => console.error(err));
