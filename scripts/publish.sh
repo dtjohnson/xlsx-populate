25b40bab9bb56a62807837cf4eabaeeb902607b3
Alexis Rico <sferadev@gmail.com>
2021-01-15 12:55:30 +0100

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e -u
 
-version=$(cat package.json | jq -r '.version')
-git tag v$version -f
-
 yarn build
 
+version=$(cat package.json | jq -r '.version')
 publish_opts=$(echo $version | grep -q beta && echo "--tag beta" || true)
 yarn publish $publish_opts --new-version $version
 
+git tag v$version -f
 git push
 git push --tags
