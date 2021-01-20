7aee4dbbfe26b842cf58b8243b99a4c99d4051c3
Alexis Rico <sferadev@gmail.com>
2021-01-20 17:25:33 +0100

    Move build to prerelease

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e -u
 
-yarn build
-
 version=$(cat package.json | jq -r '.version')
 publish_opts=$(echo $version | grep -q beta && echo "--tag beta" || true)
 yarn publish $publish_opts --new-version $version
